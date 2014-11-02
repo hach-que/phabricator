570e0d87a0f8da99aad8f0786fc38c2cab92375e
James Rhodes <jrhodes@redpointsoftware.com.au>
2014-11-02 20:34:58 +1100

    Implement `drydock ssh` for running commands against leases
    Summary:
    Ref T2015.  This allows users to run commands against a lease using the command interface, via a command line `bin/drydock ssh` workflow.  It is used like this:
    
    ```
    bin/drydock ssh --id 1234 --command 'echo test'
    ```
    
    Test Plan: Tested on a production server while using it to diagnose issues.
    
    Reviewers: #blessed_reviewers, epriestley
    
    Subscribers: epriestley, Korvin
    
    Maniphest Tasks: T2015
    
    Differential Revision: https://secure.phabricator.com/D10469

---
+++ src/applications/drydock/management/DrydockManagementSSHWorkflow.php
@@ -0,0 +1,56 @@
+<?php
+
+final class DrydockManagementSSHWorkflow
+  extends DrydockManagementWorkflow {
+
+  public function didConstruct() {
+    $this
+      ->setName('ssh')
+      ->setSynopsis('Run a command on a remote host.')
+      ->setArguments(
+        array(
+          array(
+            'name'      => 'id',
+            'param'     => 'lease',
+          ),
+          array(
+            'name'      => 'command',
+            'param'     => 'command',
+          ),
+        ));
+  }
+
+  public function execute(PhutilArgumentParser $args) {
+    $console = PhutilConsole::getConsole();
+
+    $id = $args->getArg('id');
+    if (!$id) {
+      throw new PhutilArgumentUsageException(
+        'Specify a lease ID to run the command on.');
+    }
+
+    $viewer = $this->getViewer();
+
+    $leases = id(new DrydockLeaseQuery())
+      ->setViewer($viewer)
+      ->withIDs(array($id))
+      ->execute();
+    $lease = idx($leases, $id);
+
+    if (!$lease) {
+      $console->writeErr("Lease %d does not exist!\n", $id);
+    } else if ($lease->getStatus() != DrydockLeaseStatus::STATUS_ACTIVE) {
+      $console->writeErr("Lease %d is not 'active'!\n", $id);
+    } else {
+      $interface = $lease->getInterface('command');
+      $future = $interface->getExecFuture('%C', $args->getArg('command'));
+
+      list($err, $stdout, $stderr) = $future->resolve();
+      $console->writeOut("Result: %d\n\n", $err);
+      $console->writeOut("%s\n", $stdout);
+      $console->writeErr("%s\n", $stderr);
+    }
+
+  }
+
+}
