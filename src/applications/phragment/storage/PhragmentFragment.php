4b41a4bcdf13c455c92cfbf1b15f1bdf6d00ff95
June Rhodes <jrhodes@redpointsoftware.com.au>
2015-08-25 01:28:27 +1000

    [custom/phragment/db] Set fragment to bytes255

--- src/applications/phragment/storage/PhragmentFragment.php
+++ src/applications/phragment/storage/PhragmentFragment.php
@@ -15,7 +15,7 @@ final class PhragmentFragment extends PhragmentDAO
     return array(
       self::CONFIG_AUX_PHID => true,
       self::CONFIG_COLUMN_SCHEMA => array(
-        'path' => 'text128',
+        'path' => 'bytes255',
         'depth' => 'uint32',
         'latestVersionPHID' => 'phid?',
       ),
